19b132c66421b622e16321407469f07e349a6d8b
Jovian Wang <jovian.wang@a2pg.com>
2022-06-02 13:23:47 -0400

    style

--- ui.R
+++ ui.R
@@ -4,6 +4,7 @@ ui <- fluidPage(
   # verbatimTextOutput("results"),
   # verbatimTextOutput("point"),
   titlePanel("he walkin"),
+  HTML("<h3>Try using WASD. A test project and an example of reactivity in <a href='https://shiny.rstudio.com/'>RShiny</a>.</h3>"),
   htmlOutput("board"),
   tags$script('
     downKeyCount = 0;
@@ -11,5 +12,7 @@ ui <- fluidPage(
        Shiny.onInputChange("downKey", downKeyCount++);
        Shiny.onInputChange("downKeyId", e.code);
     });'
-  )
+  ),
+  tags$style("* {font-family: 'EB Garamond';}"),
+  tags$style("h2 {font-weight: bold;}")
 )
